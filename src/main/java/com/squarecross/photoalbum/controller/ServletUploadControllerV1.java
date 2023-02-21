8cea44ce946bda3940ea308fe3bd3e6c80fe32d9
plan11plan <pmsuk7028@naver.com>
2023-02-21 20:48:31 +0900

    .

--- src/main/java/com/squarecross/photoalbum/controller/ServletUploadControllerV1.java
+++ src/main/java/com/squarecross/photoalbum/controller/ServletUploadControllerV1.java
@@ -3,8 +3,15 @@ package com.squarecross.photoalbum.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.util.Collection;
 
 @Slf4j
 @Controller
@@ -13,6 +20,16 @@ public class ServletUploadControllerV1 {
 
     @GetMapping("upload")
     public String newFile(){
+        return "upload-form";
+    }
+    @PostMapping("/upload")
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request = {}",request);
+        String itemName = request.getParameter("itemName");
+        log.info("itemNAme= {} ",itemName);
+        //여기서 중요합니다.
+        Collection<Part> parts = request.getParts(); //part가 ---xxx 파트마다 받을 수 있음.
+        log.info("parts={}",parts);
         return "upload-form";
     }
 }
