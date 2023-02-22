919ba0bfbc085c03494631aad138a15e4c196f9e
plan11plan <pmsuk7028@naver.com>
2023-02-22 16:38:22 +0900

    .

--- src/main/java/com/squarecross/photoalbum/domain/Item.java
+++ src/main/java/com/squarecross/photoalbum/domain/Item.java
@@ -2,16 +2,21 @@ package com.squarecross.photoalbum.domain;
 
 import lombok.Data;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Data
 public class Item {
     @Id @GeneratedValue
+    @Column(name="item_id")
     private Long id;
     private String itemName;
+    @OneToOne(fetch = FetchType.LAZY,mappedBy = "item2",cascade = CascadeType.ALL)
     private UploadFile attachFile;
-    private List<UploadFile> imageFiles;//이미지는 여러개 업로드 가능
+
+    @OneToMany(mappedBy = "item",cascade = CascadeType.ALL)
+    private List<UploadFile> imageFiles = new ArrayList<>();//이미지는 여러개 업로드 가능
 
 }
--- src/main/java/com/squarecross/photoalbum/domain/UploadFile.java
+++ src/main/java/com/squarecross/photoalbum/domain/UploadFile.java
@@ -2,15 +2,29 @@ package com.squarecross.photoalbum.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 public class UploadFile {
+    @Id
+    @GeneratedValue
+    @Column(name="upload_file_id")
+    private Long id;
     private String uploadFileName;
     private String storeFileName; //시스템에 저장한 파일 이름
     //사용자가 2명 이상 같은 파일명 업로드 했을 경우, 나의 디스크에 같은 파일명이면 덮어지겠죠
     // uuid로 안겹치게
 
+//    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Item item;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Item item2;
     public UploadFile(String uploadFileName, String storeFileName) {
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
     }
+    protected UploadFile(){}
 }
